be0cd36526168540787f692cd3e89b2dd2dbab5a
Korab Beqiraj <korabbeqiraj@Korabs-MBP.broadband>
2024-01-25 20:42:10 +0000

    changes to work with gateway

--- src/main/java/com/korbii/demo/TestController.java
+++ src/main/java/com/korbii/demo/TestController.java
@@ -3,14 +3,17 @@ package com.korbii.demo;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = {"*"})
+@RequestMapping(path = "hello")
 public class TestController {
 
-	@GetMapping
-	public ResponseEntity<String> testEndPoint(){
+	
+	@GetMapping("test")
+	public ResponseEntity<String> testEndPoint2(){
 		
 		return ResponseEntity.ok("successfully reached the endpoint");
 	}
